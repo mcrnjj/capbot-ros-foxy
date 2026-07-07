18d17ac9429060f7c92fc5028df9110a1ce1939b
Francisco Leay Ochoa <fleay@uc.cl>
2026-07-07 00:49:39 -0400

    Reduce power consumption

--- src/test_bot/launch/real_robot.launch.py
+++ src/test_bot/launch/real_robot.launch.py
@@ -91,18 +91,18 @@ def generate_launch_description():
             'sensor_id': 0,
             'capture_width': 1280, 'capture_height': 720,
             'output_width': 640, 'output_height': 480,
-            'framerate': 30, 'flip_method': 0,
+            'framerate': 15, 'flip_method': 0,
             'frame_id': 'camera_link_optical',
             'camera_info_url': camera_info,
             'enable_video': True,
             'host_ip': '',          # vacio => usa la env HOST_IP
             'video_port': 5000,
-            'video_bitrate_kbps': 4000,
+            'video_bitrate_kbps': 2000,
         }],
     )
 
     delayed_camera = TimerAction(
-        period=15.0,
+        period=40,
         actions=[camera]
     )
 
